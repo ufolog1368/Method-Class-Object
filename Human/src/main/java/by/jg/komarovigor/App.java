b3847232f5dee2bb0eb3f105ac4c8fba3824696f
igor <ufolog1368@gmail.com>
2020-08-26 13:26:29 +0300

    correction App

--- Human/src/main/java/by/jg/komarovigor/App.java
+++ Human/src/main/java/by/jg/komarovigor/App.java
@@ -5,8 +5,7 @@ import by.jg.komarovigor.service.HumanService;
 
 public class App {
     public static void main(String[] args) {
-        HumanService humanService = new HumanService();
-        humanService.greet();
+
 
 
     }
